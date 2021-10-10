4a6e93c2f1c4126d4ec96f858ba7fa51c6c23796
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-10-10 19:07:11 +0500

    Lesson_3, used readline method

--- Lesson_3/webinar.js
+++ Lesson_3/webinar.js
@@ -1,27 +1,34 @@
 const fs = require('fs')
-
+const fsPromises = require('fs/promises')
 const Log_file = './Lesson_3/logi.log'
 
-const data = [
+const requests = [
   '127.0.0.1 - - [30/Jan/2021:11:10:16 -0300] "GET /sitemap.xml HTTP/1.1" 200 0 "-" "curl/7.47.0"',
   '127.0.0.1 - - [30/Jan/2021:11:10:17 -0300] "GET /sitemap.xml HTTP/1.1" 200 0 "-" "curl/7.47.0"',
 ]
 
-data.forEach((log) => {
+requests.forEach((log) => {
   fs.writeFile(
     Log_file,
-    log,
+    `${log}\n`,
     {
       flag: 'a',
     },
-    (err) => console.log(err),
+    (err) => {
+      if (err) console.log
+    },
   )
 })
+// try {
+//   const data = fs.readFileSync(Log_file, {})
+//   console.log(data.toString())
+// } catch (error) {
+//   console.log(error)
+// }
 
-fs.readFile(
-  Log_file,
-  {
-    encoding: 'utf-8',
-  },
-  (err, data) => console.log(data),
-)
+// fs.readFile(Log_file, (err, data) => {
+//   if (err) console.log(err)
+//   else console.log(data.toString())
+// })
+
+fsPromises.readFile(Log_file, 'utf-8').then(console.log).catch(console.log)
