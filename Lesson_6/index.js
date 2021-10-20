e85f1b1d729ea000ab0a50ea178bd5510b8d0996
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-10-20 05:29:53 +0500

    Lesson_6: some fixes, users list online

--- Lesson_6/index.js
+++ Lesson_6/index.js
@@ -5,41 +5,50 @@ const port = 3000;
 const { Server } = require('socket.io');
 
 const server = http.createServer((req, res) => {
-  const fullPath = path.join(__dirname, './index.html');
+    const fullPath = path.join(__dirname, './index.html');
 
-  const html = fs.readFileSync(fullPath, 'utf-8');
+    const html = fs.readFileSync(fullPath, 'utf-8');
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  res.write(html);
+    res.write(html);
 
-  res.end();
+    res.end();
 });
 
 const io = new Server(server);
 
 let numberOfClients = 0;
+let clients = {};
 
 io.on('connection', (client) => {
-  ++numberOfClients;
+    ++numberOfClients;
 
-  client.broadcast.emit('number of clients', numberOfClients);
-  client.emit('number of clients', numberOfClients);
+    client.on('newClientName', (name) => {
+        clients = { ...clients, [client.id]: name };
+        const clientNameOnline = Object.values(clients);
+        client.broadcast.emit('number of clients', { numberOfClients, clientNameOnline });
+        client.emit('number of clients', { numberOfClients, clientNameOnline });
+    });
 
-  client.emit('client conneted');
+    client.on('disconnect', () => {
+        --numberOfClients;
+        const msg = {
+            message: 'disconnected',
+            name: clients[client.id],
+        };
+        client.broadcast.emit('server message', msg);
+        delete clients[client.id];
+        const clientNameOnline = Object.values(clients);
+        client.broadcast.emit('number of clients', { numberOfClients, clientNameOnline });
+    });
 
-  client.on('disconnect', () => {
-    --numberOfClients;
-    client.broadcast.emit('client disconneted', numberOfClients);
-    client.emit('client disconneted', numberOfClients);
-  });
-
-  client.on('chat message', (msg) => {
-    client.broadcast.emit('server message', msg);
-    client.emit('server message', msg);
-  });
+    client.on('chat message', (msg) => {
+        client.broadcast.emit('server message', msg);
+        client.emit('server message', msg);
+    });
 });
 
 server.listen(port, () => {
-  console.log(`listening on *:${port}`);
+    console.log(`listening on *:${port}`);
 });
